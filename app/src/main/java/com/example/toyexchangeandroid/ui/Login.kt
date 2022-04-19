476874fd036d020716a8af0fb6e1e5a4356fdb60
med wajdi brahem <wajdibrahem74@gmail.com>
2022-04-20 00:30:47 +0100

    Login and SignUp done with input controls

--- app/src/main/java/com/example/toyexchangeandroid/ui/Login.kt
+++ app/src/main/java/com/example/toyexchangeandroid/ui/Login.kt
@@ -1,29 +1,45 @@
 package com.example.toyexchangeandroid.ui
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.TextView
 import android.widget.Toast
 import com.example.toyexchangeandroid.R
 import com.example.toyexchangeandroid.service.ApiService
 import com.example.toyexchangeandroid.service.ClientService
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+const val PREF_NAME = "DATA_CV_PREF"
+const val email = "email"
+const val password = "password"
+const val IS_REMEMBRED = "remembred"
 
 class Login : AppCompatActivity() {
     private var btnSubmit: Button? = null
     private var toSignUp: TextView? = null
     private var toForgotPassword: TextView? = null
-    var txtEmail: TextInputEditText? = null
-    var txtPassword: TextInputEditText? = null
 
+    private var txtEmail: TextInputEditText? = null
+    private var txtPassword: TextInputEditText? = null
+
+    lateinit var cbRememberMe: CheckBox
+    lateinit var mSharedPref: SharedPreferences
+
+    private var txtLayoutEmail: TextInputLayout? = null
+    private var txtLayoutPassword: TextInputLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,15 +51,49 @@ class Login : AppCompatActivity() {
         toSignUp = findViewById(R.id.ToSignUp)
         toForgotPassword = findViewById(R.id.ToForgotPassword)
 
+        txtLayoutEmail = findViewById(R.id.txtLayoutEmail)
+        txtLayoutPassword = findViewById(R.id.txtLayoutPassword)
+        cbRememberMe = findViewById(R.id.cbRememberMe)
+        mSharedPref = getSharedPreferences(PREF_NAME, MODE_PRIVATE);
+
         //onClick btn
         btnSubmit!!.setOnClickListener {
+            txtLayoutEmail!!.error = null
+            txtLayoutPassword!!.error = null
+
+            if (txtEmail?.text!!.isEmpty()) {
+                txtLayoutEmail!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+            if (!isEmailValid(txtEmail?.text.toString())){
+                txtLayoutEmail!!.error = "Check your email !"
+
+                return@setOnClickListener
+            }
+            if (txtPassword?.text!!.isEmpty()) {
+                txtLayoutPassword!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+
+            mSharedPref.edit().apply {
+                putString(email, txtEmail!!.text.toString())
+                putString(password, txtPassword!!.text.toString())
+                putBoolean(IS_REMEMBRED, cbRememberMe.isChecked)
+
+            }.apply()
+
+            if (cbRememberMe.isChecked){
+                mSharedPref.edit().apply{
+                    putBoolean(IS_REMEMBRED, cbRememberMe.isChecked)
+                }.apply()
+            }
+
             ApiService.CLIENT_SERVICE.login(
                 ClientService.LoginBody(
                     txtEmail!!.text.toString(),
                     txtPassword!!.text.toString()
                 )
-            )
-                .enqueue(
+            ).enqueue(
                     object : Callback<ClientService.ClientResponse> {
                         override fun onResponse(
                             call: Call<ClientService.ClientResponse>,
@@ -87,4 +137,10 @@ class Login : AppCompatActivity() {
         });
 
     }
+
+    fun isEmailValid(email: String?): Boolean {
+        return !(email == null || TextUtils.isEmpty(email)) && Patterns.EMAIL_ADDRESS.matcher(email)
+            .matches()
+    }
+
 }
--- app/src/main/java/com/example/toyexchangeandroid/ui/SignUp.kt
+++ app/src/main/java/com/example/toyexchangeandroid/ui/SignUp.kt
@@ -3,12 +3,15 @@ package com.example.toyexchangeandroid.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
+import android.util.Patterns
 import android.widget.Button
 import com.example.toyexchangeandroid.R
 import com.example.toyexchangeandroid.service.ApiService
 import com.example.toyexchangeandroid.service.ClientService
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,22 +20,61 @@ import retrofit2.Response
 class SignUp : AppCompatActivity() {
 
     var txtFullName : TextInputEditText? = null
+    var txtPhoneNumber: TextInputEditText? = null
     var txtEmail: TextInputEditText? = null
     var txtPassword: TextInputEditText? = null
-    var txtPhoneNumber: TextInputEditText? = null
     var btnSubmit: Button? = null
+
+    private var txtLayoutFullName: TextInputLayout? = null
+    private var txtLayoutPhoneNumber: TextInputLayout? = null
+    private var txtLayoutEmail: TextInputLayout? = null
+    private var txtLayoutPassword: TextInputLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
         txtFullName= findViewById(R.id.txtFullName)
+        txtPhoneNumber = findViewById(R.id.txtPhoneNumber)
         txtEmail = findViewById(R.id.txtEmail)
         txtPassword = findViewById(R.id.txtPassword)
-        txtPhoneNumber = findViewById(R.id.txtPhoneNumber)
         btnSubmit = findViewById(R.id.btnSubmit)
 
+        txtLayoutFullName = findViewById(R.id.txtLayoutFullName)
+        txtLayoutPhoneNumber = findViewById(R.id.txtLayoutPhoneNumber)
+        txtLayoutEmail = findViewById(R.id.txtLayoutEmail)
+        txtLayoutPassword = findViewById(R.id.txtLayoutPassword)
+
         btnSubmit!!.setOnClickListener {
+
+            txtLayoutFullName!!.error = null
+            txtLayoutPhoneNumber!!.error = null
+            txtLayoutEmail!!.error = null
+            txtLayoutPassword!!.error = null
+
+            if (txtFullName?.text!!.isEmpty()) {
+                txtLayoutFullName!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+            if (txtPhoneNumber?.text!!.isEmpty()) {
+                txtLayoutPhoneNumber!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+            if (txtEmail?.text!!.isEmpty()) {
+                txtLayoutEmail!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+            if (!isEmailValid(txtEmail?.text.toString())){
+                txtLayoutEmail!!.error = "Check your email !"
+
+                return@setOnClickListener
+            }
+            if (txtPassword?.text!!.isEmpty()) {
+                txtLayoutPassword!!.error = "must not be empty"
+                return@setOnClickListener
+            }
+
+
             ApiService.CLIENT_SERVICE.register(
                 ClientService.ClientBody(
                     txtFullName!!.text.toString() ,
@@ -40,16 +82,14 @@ class SignUp : AppCompatActivity() {
                     txtPassword!!.text.toString(),
                     txtPhoneNumber!!.text.toString(),
                 )
-            )
-                .enqueue(
+            ).enqueue(
                     object : Callback<ClientService.ClientResponse> {
                         override fun onResponse(
                             call: Call<ClientService.ClientResponse>,
                             response: Response<ClientService.ClientResponse>
                         ) {
                             if (response.code() == 200) {
-                                val intent =
-                                    Intent(this@SignUp, Login::class.java)
+                                val intent = Intent(this@SignUp, Login::class.java)
                                 startActivity(intent)
                                 finish()
                             } else {
@@ -67,5 +107,13 @@ class SignUp : AppCompatActivity() {
                 )
         }
 
+
     }
+
+    fun isEmailValid(email: String?): Boolean {
+        return !(email == null || TextUtils.isEmpty(email)) && Patterns.EMAIL_ADDRESS.matcher(email)
+            .matches()
+    }
+
+
 }
